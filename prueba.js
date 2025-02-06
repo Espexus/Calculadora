bed37775a1c983c9c0d5ae41a371c9169efe6c10
Espexus <jesusespinosa10usb@gmail.com>
2025-02-06 18:17:39 -0500

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,31 @@
+function suma (n1, n2){
+    var n1= parseFloat(document.getElementById("n1").value);
+    var n2= parseFloat(document.getElementById("n2").value);
+    var resultado = n1 + n2;
+
+    return resultado
+}
+
+function resta (n1, n2){
+    var n1= parseFloat(document.getElementById("n1").value);
+    var n2= parseFloat(document.getElementById("n2").value);
+    var resultado = n1 - n2;
+
+    return resultado
+}
+
+function multiplicacion (n1, n2){
+    var n1= parseFloat(document.getElementById("n1").value);
+    var n2= parseFloat(document.getElementById("n2").value);
+    var resultado = n1 * n2;
+
+    return resultado
+}
+
+function division (n1, n2){
+    var n1= parseFloat(document.getElementById("n1").value);
+    var n2= parseFloat(document.getElementById("n2").value);
+    var resultado = n1 / n2;
+
+    return resultado
+}
